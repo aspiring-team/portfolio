c024a124551f03437eded2646f2d07afe1e73b7c
zuhalal <zuhalalimulhadi19@gmail.com>
2023-07-22 06:47:35 +0700

    fix: change section component into memo

--- components/section/Section.tsx
+++ components/section/Section.tsx
@@ -3,7 +3,7 @@
 import { AlignLeftIcon, ImagePlusIcon } from "@/icons";
 import { AddIcon } from "@/icons/AddIcon";
 import { StarIcon } from "@/icons/StarIcon";
-import React from "react";
+import React, { memo } from "react";
 import { Tooltip } from "../elements";
 
 type SectionProps = {
@@ -13,40 +13,41 @@ type SectionProps = {
   type?: string;
 };
 
-export const Section: React.FC<SectionProps> = ({
-  title,
-  content,
-  slug,
-  type,
-}) => {
-  return (
-    <section className="flex flex-col-reverse items-start space-y-1 md:flex-row md:items-end md:space-x-3 md:space-y-0">
-      <section className="flex flex-row-reverse md:flex-col md:space-y-3">
-        <Tooltip text="Ask AI" icon={StarIcon}>
-          <div className="flex space-y-1">
-            <StarIcon className="h-6 w-6" />
-            <p className="p5 text-gray-700">Ask AI</p>
-          </div>
-        </Tooltip>
-        <div className="mr-20 md:mr-0">
-          <Tooltip text="More" icon={AddIcon}>
-            <div>
-              <div className="flex space-y-1">
-                <AlignLeftIcon className="h-6 w-6" />
-                <p className="p5 text-gray-700">Add Section</p>
-              </div>
-              <div className="flex space-y-1">
-                <ImagePlusIcon className="h-6 w-6" />
-                <p className="p5 text-gray-700">Add Image</p>
-              </div>
+const Section: React.FC<SectionProps> = memo(
+  ({ title, content, slug, type }) => {
+    return (
+      <section className="flex flex-col-reverse items-start space-y-1 md:flex-row md:items-end md:space-x-3 md:space-y-0">
+        <section className="flex flex-row-reverse md:flex-col md:space-y-3">
+          <Tooltip text="Ask AI" icon={StarIcon}>
+            <div className="flex space-y-1">
+              <StarIcon className="h-6 w-6" />
+              <p className="p5 text-gray-700">Ask AI</p>
             </div>
           </Tooltip>
-        </div>
+          <div className="mr-20 md:mr-0">
+            <Tooltip text="More" icon={AddIcon}>
+              <div>
+                <div className="flex space-y-1">
+                  <AlignLeftIcon className="h-6 w-6" />
+                  <p className="p5 text-gray-700">Add Section</p>
+                </div>
+                <div className="flex space-y-1">
+                  <ImagePlusIcon className="h-6 w-6" />
+                  <p className="p5 text-gray-700">Add Image</p>
+                </div>
+              </div>
+            </Tooltip>
+          </div>
+        </section>
+        <section className="flex flex-col space-y-1">
+          <p className="p1 font-bold text-gray-900">{title}</p>
+          <p className="p3 text-gray-900">{content}</p>
+        </section>
       </section>
-      <section className="flex flex-col space-y-1">
-        <p className="p1 font-bold text-gray-900">{title}</p>
-        <p className="p3 text-gray-900">{content}</p>
-      </section>
-    </section>
-  );
-};
+    );
+  }
+);
+
+Section.displayName = "Section";
+
+export { Section };
