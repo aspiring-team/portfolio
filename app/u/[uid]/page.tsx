8cf14b3df68170f1614ccf36be530e2c1bab0718
zuhalal <zuhalalimulhadi19@gmail.com>
2023-07-23 05:48:52 +0700

    feat: add loading and not found in profile

--- app/u/[uid]/page.tsx
+++ app/u/[uid]/page.tsx
@@ -12,13 +12,14 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "@/utils";
 import { PortfolioConverter, ProfileConverter } from "@/models";
 import { collection, query, where } from "firebase/firestore";
+import Image from "next/image";
 
 const ProfilePage = ({ params: { uid } }: { params: { uid: string } }) => {
-  const [profile] = useDocumentData(
+  const [profile, loadingProfile] = useDocumentData(
     uid ? doc(db, "profiles", uid).withConverter(ProfileConverter) : null
   );
 
-  const [portfolios] = useCollectionData(
+  const [portfolios, loadingPorto] = useCollectionData(
     uid
       ? query(
           collection(db, "portfolios").withConverter(PortfolioConverter),
@@ -31,23 +32,43 @@ const ProfilePage = ({ params: { uid } }: { params: { uid: string } }) => {
     <main className="flex h-full flex-col">
       <Navbar className="sticky top-0 z-10 shadow-lg" />
       <div className="container flex min-h-[calc(100vh-73px)] flex-col items-center space-y-16 py-10">
-        <HeroProfile profile={profile} />
+        {loadingPorto || loadingProfile ? (
+          <span className="loading loading-spinner" />
+        ) : (
+          <>
+            <HeroProfile profile={profile} />
 
-        <div className="w-full space-y-6 border-t-2 border-gray-300 pt-24">
-          <h6 className="h6 font-bold text-black">My Projects</h6>
-          <div className="flex flex-col space-y-10 md:space-y-24">
-            {portfolios?.map((p) => (
-              <ProjectCard
-                key={p.slug}
-                uid={p.uid}
-                slug={p.slug}
-                image={p.cover}
-                title={p.title}
-                description={p.summary}
-              />
-            ))}
-          </div>
-        </div>
+            <div className="w-full space-y-6 border-t-2 border-gray-300 pt-24">
+              <h6 className="h6 font-bold text-black">Projects</h6>
+              <div className="flex flex-col space-y-10 md:space-y-24">
+                {portfolios && portfolios?.length > 0 ? (
+                  portfolios?.map((p) => (
+                    <ProjectCard
+                      key={p.slug}
+                      uid={p.uid}
+                      slug={p.slug}
+                      image={p.cover}
+                      title={p.title}
+                      description={p.summary}
+                    />
+                  ))
+                ) : (
+                  <div className="flex flex-col items-center justify-center md:flex-row">
+                    <Image
+                      src="/coming-soon.svg"
+                      alt="No Projects"
+                      width={200}
+                      height={200}
+                    />
+                    <h6 className="h6 text-black font-bold">
+                      Currently, there are no project here.
+                    </h6>
+                  </div>
+                )}
+              </div>
+            </div>
+          </>
+        )}
       </div>
       <SocialLinks profile={profile} />
     </main>
