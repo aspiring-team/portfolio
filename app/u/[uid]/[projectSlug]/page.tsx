eb468c5aadeb1416bc0eaf81615e36dd0d8b963c
zuhalal <zuhalalimulhadi19@gmail.com>
2023-07-23 06:39:11 +0700

    fix: loading position in project detail

--- app/u/[uid]/[projectSlug]/page.tsx
+++ app/u/[uid]/[projectSlug]/page.tsx
@@ -9,7 +9,6 @@ import { PortfolioConverter } from "@/models";
 import { db } from "@/utils";
 
 import Image from "next/image";
-import Error from "next/error";
 
 import { Footer, Navbar } from "@/components";
 import { ProjectProfile } from "@/modules";
@@ -38,49 +37,54 @@ const ProjectDetailPage = ({
     return portfolios.find((p) => p.slug === projectSlug);
   }, [portfolios, portfoliosLoading, projectSlug]);
 
-  if (portfoliosLoading || !portfolio) {
-    return <span className="loading loading-spinner" />;
-  }
-
   return (
     <main className="flex h-full flex-col">
       <Navbar className="sticky top-0 z-10 shadow-lg" />
       <div className="container flex min-h-[calc(100vh-73px)] flex-col items-center space-y-9 py-10">
-        <ProjectProfile title={portfolio.title} />
-        <div className="flex w-full justify-center">
-          <div className="relative mb-8 aspect-square h-[480px] w-[100%] place-self-start overflow-clip rounded-xl border md:mb-0 ">
-            <Image
-              className="object-cover"
-              alt="project cover"
-              src={portfolio.cover}
-              fill
-            />
-          </div>
-        </div>
+        {portfoliosLoading || !portfolio ? (
+          <span className="loading loading-spinner" />
+        ) : (
+          <>
+            <ProjectProfile title={portfolio.title} />
+            <div className="flex w-full justify-center">
+              <div className="relative mb-8 aspect-square h-[480px] w-[100%] place-self-start overflow-clip rounded-xl border md:mb-0 ">
+                <Image
+                  className="object-cover"
+                  alt="project cover"
+                  src={portfolio.cover}
+                  fill
+                />
+              </div>
+            </div>
 
-        <section className="flex flex-col space-y-4">
-          {portfolio.sections?.map((portfolio) => (
-            <section key={portfolio.title}>
-              <>
-                {portfolio.image ? (
-                  // eslint-disable-next-line @next/next/no-img-element
-                  <img
-                    className="aspect-video max-w-full rounded-xl object-cover"
-                    src={portfolio.image}
-                    alt="cover section"
-                  />
-                ) : (
+            <section className="flex flex-col space-y-4">
+              {portfolio.sections?.map((portfolio) => (
+                <section key={portfolio.title}>
                   <>
-                    <p className="p1 font-bold text-gray-900">
-                      {portfolio.title}
-                    </p>
-                    <Editor initialContent={portfolio.content ?? ""} readOnly />
+                    {portfolio.image ? (
+                      // eslint-disable-next-line @next/next/no-img-element
+                      <img
+                        className="aspect-video max-w-full rounded-xl object-cover"
+                        src={portfolio.image}
+                        alt="cover section"
+                      />
+                    ) : (
+                      <>
+                        <p className="p1 font-bold text-gray-900">
+                          {portfolio.title}
+                        </p>
+                        <Editor
+                          initialContent={portfolio.content ?? ""}
+                          readOnly
+                        />
+                      </>
+                    )}
                   </>
-                )}
-              </>
+                </section>
+              ))}
             </section>
-          ))}
-        </section>
+          </>
+        )}
       </div>
       <Footer />
     </main>
